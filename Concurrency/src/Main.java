cb222517abbf59deaa53d989cc6489f0f2734ccb
Minura Hajisoy <minura.hajisoy@gmail.com>
2023-11-06 15:49:36 +0400

    adding command line

--- Concurrency/src/Main.java
+++ Concurrency/src/Main.java
@@ -1,5 +1,11 @@
+import static java.lang.Integer.parseInt;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Number of Command Line Argument = "+args.length);
+        String fileName = args[0];
+        int squareSize = parseInt(args[1]);
+        String processingMode = args[2];
+        System.out.println(args[0]+args[1]+args[2]);
     }
 }
